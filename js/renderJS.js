e01c752e35adc1ea0d30370a06064699f6f0081b
joshbyrom <joshbyrom@gmail.com>
2013-05-17 14:14:34 -0400

    initial project files and pattern

---
+++ js/renderJS.js
@@ -0,0 +1,11 @@
+﻿(function (window, undefined) { // window == context
+    var renderJS = function ctor() {
+
+    };
+
+    renderJS.prototype.test = function test_function() {
+        console.log("Hello, from renderJS");
+    };
+    
+    window.renderJS = new renderJS();
+})(this);
